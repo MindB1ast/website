7010374c1cbc3fc3ec012f35694b32367ddd0731
nikitasuvorov2 nikitasuvorov2 <lialia2000@inbox.ru>
2022-02-11 22:30:33 +0500

    excel form added

--- sait.py
+++ sait.py
@@ -12,10 +12,16 @@ import keras
 from keras.models import Sequential, Model
 from re import match
 from check import check,load_data_one
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import pandas as pd
+from flask import send_from_directory
 
 app = Flask(__name__,template_folder="templ")
 
-
+ALLOWED_EXTENSIONS = {'xlsx', 'csv', 'xls'}
+app.config['UPLOAD_FOLDER']="/uploads"
 from flask import Flask, request, render_template
 @app.route('/')
 def index():
@@ -25,7 +31,7 @@ def index():
 def num_text():
     json_data = request.get_json()
     # Проверяем наличие необходимого ключа в запросе
-    if 'number' not in json_data:
+    if 'number' not in json_data or 'series' not in json_data:
         return jsonify(str='something wrong')
     # Достаем пришедшие данные из запроса
     number = json_data['number']
@@ -39,6 +45,30 @@ def num_text():
 
     return jsonify(str=text)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/excel/', methods=['POST'])
+def work_with_excel():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':#Если какой либо файл отправили
+            uploaded_file.save(os.path.join(os.getcwd(),'uploads','file.'+uploaded_file.filename.rsplit('.', 1)[1].lower())) #'1'+uploaded_file.filename.rsplit('.', 1)[1].lower())) #os.getcwd() os.path.join(path, *paths)
+            sheets=pd.read_excel('./uploads/file.'+uploaded_file.filename.rsplit('.', 1)[1].lower())
+            buffer_array=[]
+            for i,x in enumerate(sheets.values):
+                if (type(x[0])==np.int64 and type(x[1])==np.int64 and len(str(x[0]))==4 and len(str(x[1]))==6):
+                   buffer_array.append(check(x[0],x[1]))#sheets.values[2][i]=check(x[0],x[1])
+            sheets["Вердикт"]=buffer_array
+            sheets.to_excel('./uploads/file.'+uploaded_file.filename.rsplit('.', 1)[1].lower())
+            #print(sheets)
+            
+        return send_from_directory(path='.', directory='uploads', filename='file.xlsx')#""#redirect(url_for('download')) #<a href="{{ url_for('download', filename="downloadFile.txt") }}">File</a>
+    return render_template('index.html')
+
+
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
